d76663cbdd0cc09c24f492acc28a13ce4112dc32
Jofich <121862759+Jofich@users.noreply.github.com>
2024-04-28 12:26:22 +0300

    add begin(), end(), pop_back(), template construction

--- inc/myVector.h
+++ inc/myVector.h
@@ -1,25 +1,37 @@
 // my Vector class 
-namespace shp 
+#include < cassert>
+namespace shp
 {
-    class vector 
+	template <typename T>
+	class vector
 	{
 	private:
-		int all{};
+		int m_capacity{};
 		int m_size{};
-		int* data = new int[all];
+		T* data = nullptr;// new T[m_capacity];
 	public:
-
-		int operator[](int ind) {
-			if(ind < m_size && ind >=0)	return data[ind];
-		}
-
-		vector(int size_array) {
+		vector(int size_array, T fill_symbol =0) {
 			m_size = size_array;
-			all = size_array * 2;
-			data = new int[all];
-			fill(data, all);
+			m_capacity = size_array * 2;
+			data = new T[m_capacity];
+			fill(data, m_size,fill_symbol);
 		}
-		void fill(int* array, int array_size, int fill_symbol=0) {
+		//TODO конструктор копирования 
+		vector& operator=(const vector&& copy) {
+			delete data;
+			data = copy.data;
+			m_size = copy.m_size;
+			m_capacity = m_size;
+		}
+		T operator[](int ind) {
+			assert(("ind must be in range [0; size-1]",ind < m_size&& ind >= 0));
+			return data[ind];
+
+		}
+		int capacity() {
+			return m_capacity;
+		}
+		void fill(T* array, int array_size, T fill_symbol = 0) {
 			for (int i = 0; i < array_size; i++) {
 				array[i] = fill_symbol;
 			}
@@ -27,25 +39,38 @@ namespace shp
 		int size() {
 			return this->m_size;
 		}
-		void push_back(int number) {
-			if (m_size + 1 > all) {
+		void push_back(T number) {
+			if (m_size + 1 > m_capacity) {
 				resize();
 			}
-			data[m_size] = number;
+			data[m_size-1] = number;
 			m_size++;
 		}
-
+		T back() {
+			return data[m_size-1];
+		}
+		void pop_back() {
+			
+			data[m_size-1] = 0;
+			m_size -= 1;
+		}
+		T* begin() {
+			return data;
+		}
+		T* end() {
+			return data + m_size;
+		}
 		void resize() {
-			int new_size = all * 2;
-			int* resized_data = new int[new_size];
+			int new_size = m_capacity * 2;
+			T* resized_data = new T[new_size];
 			fill(resized_data, new_size);
-			for (int i = 0; i < all; i++) {
+			for (int i = 0; i < m_capacity; i++) {
 				resized_data[i] = data[i];
 			}
 			delete[] data;
 			data = nullptr;
-			all = new_size;
-			data = new int[new_size];
+			m_capacity = new_size;
+			data = new T[new_size];
 			fill(data, new_size);
 			for (int i = 0; i < new_size; i++) {
 				data[i] = resized_data[i];
